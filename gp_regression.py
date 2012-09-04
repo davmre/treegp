324651c20dd01a6b77a73cabd1318ddffd7a02c0
David Moore <davmre@gmail.com>
2012-09-03 20:49:00 -0400

    a whole mess of changes from sigvisa devel tree

--- gp_regression.py
+++ gp_regression.py
@@ -9,7 +9,14 @@ import scipy.linalg, scipy.optimize
 import kernels
 
 def sq_loss(y1, y2):
-    return np.sum(.5 * np.abs(y1-y2)**2)
+    return np.sum(np.abs(y1-y2)**2)
+
+def rms_loss(y1, y2):
+    return np.sqrt(np.mean((y1-y2)**2))
+
+def abs_loss(y1, y2):
+    return np.sum(np.abs(y1-y2))
+
 
 def log_det(X):
     w = np.linalg.eigvalsh(X)
@@ -20,7 +27,7 @@ def log_det(X):
     # kernel_params_values: a list of lists, with with ith list giving values to try for the ith kernel param
     # sigma_values: list of values to try for sigma
     # if either "values" param is None, don't optimize over that param
-def cross_validate(X, y, kernel="se", folds=-1, kernel_params_values=None, sigma_values=None):
+def cross_validate(X, y, kernel="se", folds=-1, kernel_params_values=None, kernel_extra=None, sigma_values=None):
 
     f = open("cv_results.log", 'a')
 
@@ -30,20 +37,20 @@ def cross_validate(X, y, kernel="se", folds=-1, kernel_params_values=None, sigma
     best_params = np.ones((len(kernel_params_values),))
     best_sigma = 1
     best_param_loss = np.float("inf")
-        
+
     for params in itertools.product(*kernel_params_values):
         se_kernel = kernels.SEKernel(params)
         K = se_kernel(X, X)
         for sigma in sigma_values:
             Ks = K + np.eye(K.shape[0])*(sigma**2)
-            print params[0], sigma, 
-            loss, train_loss, baseline_loss = test_kfold(X, y, folds, "se_noiseless", params, Ks, sq_loss, train_loss=True)
+            print params[0], sigma,
+            loss, train_loss, baseline_loss = test_kfold(X, y, folds, "se_noiseless", params, kernel_extra, Ks, sq_loss, train_loss=True)
             print "give loss", loss, "training loss", train_loss, "baseline", baseline_loss
             f.write("%f %f %f %f\n" % (params[0], sigma, loss, train_loss))
             f.flush()
     f.close()
 
-def test_kfold(X, y, folds, kernel, kernel_params, K, loss_fn, train_loss=False):
+def test_kfold(X, y, folds, kernel, kernel_params, kernel_extra, K, loss_fn, train_loss=False):
     n = X.shape[0]
     foldsize = np.floor(n/folds)
 
@@ -52,32 +59,41 @@ def test_kfold(X, y, folds, kernel, kernel_params, K, loss_fn, train_loss=False)
     baseline_loss = 0
 
     if train_loss or (K is None):
-        gp = GaussianProcess(X=X, y=y, kernel=kernel, kernel_params=kernel_params, K=K, inv=False)
+        gp = GaussianProcess(X=X, y=y, kernel=kernel, kernel_params=kernel_params, kernel_extra=kernel_extra, K=K, inv=False)
         predictions = gp.predict(X)
         tl = loss_fn(predictions, y)
+        print "train pred mean", np.mean(predictions), "true mean", np.mean(y)
         K = gp.K
 
+    kfold_predictions = np.zeros((n,))
+
     for i in range(folds):
-        train = np.concatenate([np.arange(0, i*foldsize, dtype=np.uint), np.arange((i+1)*foldsize, n, dtype=np.uint)])
-        validate = np.arange(i*foldsize, (i+1)*foldsize, dtype=np.uint)
-        gp = GaussianProcess(X=X[train, :], y=y[train,:], kernel=kernel, kernel_params=kernel_params, K=K[train, :][:, train], inv=False)
+        foldstart = i*foldsize
+        foldend = (i+1)*foldsize if i < folds-1 else n
+        print "fold %f from %d to %d, size (%d)" % (i, foldstart, foldend, foldend-foldstart)
+        train = np.concatenate([np.arange(0, foldstart, dtype=np.uint), np.arange(foldend, n, dtype=np.uint)])
+        validate = np.arange(foldstart, foldend, dtype=np.uint)
+        gp = GaussianProcess(X=X[train, :], y=y[train,:], kernel=kernel, kernel_params=kernel_params, kernel_extra=kernel_extra, K=K[train, :][:, train], inv=False)
         predictions = gp.predict(X[validate,:])
+        kfold_predictions[foldstart:foldend] = predictions
         loss += loss_fn(predictions, y[validate])
         baseline_loss += loss_fn(np.mean(y[train,:]), y[validate,:])
+        print "pred mean", np.mean(predictions), "train mean", np.mean(y[train,:]), "validate mean", np.mean(y[validate,:])
+        print "pred loss", loss_fn(predictions, y[validate]), "baseline_loss", loss_fn(np.mean(y[train,:]), y[validate,:])
 
     if train_loss:
-        return loss/float(foldsize*folds), tl/float(n), baseline_loss/float(n)
+        return loss/float(foldsize*folds), tl/float(n), baseline_loss/float(foldsize*folds), kfold_predictions
     else:
-        return loss/float(n)
+        return loss/float(n), kfold_predictions
 
 class GaussianProcess:
 
-    def __init__(self, X=None, y=None, kernel="se", kernel_params=(1, 1,), kernel_priors = None, kernel_extra = None, K=None, inv=True, mean="zero", fname=None):
+    def __init__(self, X=None, y=None, kernel="se", kernel_params=(1, 1,), kernel_priors = None, kernel_extra = None, K=None, inv=True, mean="constant", ignore_pos_def_errors=False, fname=None):
         if fname is not None:
             self.load_trained_model(fname)
         else:
             self.kernel_name = kernel
-            self.kernel_params = kernel_params
+            self.kernel_params = np.asarray(kernel_params)
             self.kernel_priors = kernel_priors
             self.kernel = kernels.setup_kernel(kernel, kernel_params, kernel_extra, kernel_priors)
             self.X = X
@@ -85,7 +101,7 @@ class GaussianProcess:
 
             if mean == "zero":
                 self.mu = 0
-                self.y = y 
+                self.y = y
             if mean == "constant":
                 self.mu = np.mean(y)
                 self.y = y - self.mu
@@ -102,14 +118,30 @@ class GaussianProcess:
             self.L = None
             self.alpha = None
             self.Kinv = None
+            self.posdef = True
             try:
                 self.L = scipy.linalg.cholesky(self.K, lower=True)
                 self.alpha = scipy.linalg.cho_solve((self.L, True), self.y)
-            except:
-                print traceback.format_exc()
-                u,v = np.linalg.eig(self.K)
-                print self.K, u
-                sys.exit(1)
+            except np.linalg.linalg.LinAlgError:
+
+                if ignore_pos_def_errors:
+                    self.posdef=False
+                    return
+                else:
+                     raise
+
+#                     u,v = np.linalg.eig(self.K)
+#                     print self.K, u
+
+#                     import pdb
+#                     pdb.set_trace()
+#                     sys.exit(1)
+            except ValueError:
+                raise
+
+#                import pdb
+#                pdb.set_trace()
+#                self.K = self.kernel(X, X)
 
             if inv:
                 self.__invert_kernel_matrix()
@@ -118,30 +150,90 @@ class GaussianProcess:
         if self.Kinv is None and self.L is not None:
             invL = scipy.linalg.inv(self.L)
             self.Kinv = np.dot(invL.T, invL)
-        
+
+    def sample(self, X1):
+        X1 = np.array(X1)
+        if len(X1.shape) == 1:
+            X1 = np.reshape(X1, (1, -1))
+
+        (n,d) = X1.shape
+        means = self.predict(X1)
+        K = self.variance(X1)
+
+        L = scipy.linalg.cholesky(K, lower=True)
+        samples = np.random.randn(n, 1)
+        samples = means + np.dot(L, samples)
+        return samples
+
     def predict(self, X1):
+        if not self.posdef:
+            return self.mu
+
         K = self.kernel(self.X, X1)
         return self.mu + np.dot(K.T, self.alpha)
 
     def variance(self, X1):
+        if not self.posdef:
+            return 0
+
         self.__invert_kernel_matrix()
         K = self.kernel(self.X, X1)
         return self.kernel(X1,X1) - np.dot(K.T, np.dot(self.Kinv, K))
 
-    def log_likelihood(self):
+    def posterior_log_likelihood(self, X1, y):
+        y = np.array(y)
+        if len(y.shape) == 0:
+            n = 1
+        else:
+            n = len(y)
+
+        if not self.posdef:
+            return np.float('-inf')
+
         self.__invert_kernel_matrix()
 
-        # the determinant of a symmetric pos. def. matrix is the product of squares of the diagonal elements of the Cholesky factor 
+        K = self.variance(X1)
+        y = y-self.predict(X1)
+
+        if n==1:
+            var = K[0,0]
+            ll1 = - .5 * ((y)**2 / var + np.log(2*np.pi*var) )
+
+        if K[0,0] < 0:
+            import pdb
+            pdb.set_trace()
+
+
+        L = scipy.linalg.cholesky(K, lower=True)
+        ld2 = np.log(np.diag(L)).sum()
+        alpha = scipy.linalg.cho_solve((L, True), y)
+        ll =  -.5 * (np.dot(y.T, alpha) + n * np.log(2*np.pi)) - ld2
+        return ll
+
+
+    def log_likelihood(self):
+        if not self.posdef:
+            return np.float('-inf')
+
+        self.__invert_kernel_matrix()
+
+        # the determinant of a symmetric pos. def. matrix is the product of squares of the diagonal elements of the Cholesky factor
         ld2 = np.log(np.diag(self.L)).sum()
         ll =  -.5 * (np.dot(self.y.T, self.alpha) + self.n * np.log(2*np.pi)) - ld2
 #        print "params", self.kernel_params
+
 #        print "returning ll %f = -.5 * (%f + %f + %f)" % (ll, np.dot(self.y.T, self.alpha), ld, self.n * np.log(2*np.pi))
 
         return ll
 
     def log_likelihood_gradient(self):
-        self.__invert_kernel_matrix()
         grad = np.zeros(self.kernel_params.shape)
+
+        if not self.posdef:
+            return grad
+
+        self.__invert_kernel_matrix()
+
         for i,p in enumerate(self.kernel_params):
 
             dKdi = self.kernel.derivative_wrt_i(i, self.X, self.X)
@@ -155,7 +247,7 @@ class GaussianProcess:
 #            print "alt is %f" % (alt)
 
             grad[i] = dlldi
-        print self.kernel_params, "returning grad", grad
+#        print self.kernel_params, "returning grad", grad
         return grad
 
     def save_trained_model(self, filename):
@@ -177,7 +269,7 @@ class GaussianProcess:
         self.K = npzfile['K']
 
         self.n = self.X.shape[0]
-        self.kernel = kernels.setup_kernel(kernel_name, kernel_params)        
+        self.kernel = kernels.setup_kernel(kernel_name, kernel_params)
 #    def validation_loss(self, trainIdx, valIdx, kernel_params, loss_fn):
 
 def gp_ll(X, y, kernel, kernel_params, kernel_extra):
@@ -198,7 +290,7 @@ def gp_grad(X, y, kernel, kernel_params, kernel_extra):
 
 def gp_nll_ngrad(X, y, kernel, kernel_params, kernel_extra, kernel_priors):
     try:
-        gp = GaussianProcess(X=X, y=y, kernel=kernel, kernel_params=kernel_params, kernel_extra=kernel_extra, kernel_priors=kernel_priors)
+        gp = GaussianProcess(X=X, y=y, kernel=kernel, kernel_params=kernel_params, kernel_extra=kernel_extra, kernel_priors=kernel_priors, ignore_pos_def_errors=True)
         nll = -1 * gp.log_likelihood()
         ngrad = -1 * gp.log_likelihood_gradient()
 
@@ -210,9 +302,13 @@ def gp_nll_ngrad(X, y, kernel, kernel_params, kernel_extra, kernel_priors):
         nll += npll
         ngrad += npgrad
 
+        print "optimizing params", kernel_params, "ll", -1 *nll, "grad", ngrad
+
     except np.linalg.linalg.LinAlgError:
+        print "warning: lin alg error in likelihood computation (%f, %f), returning inf" % (nll, npll)
         nll = np.float("inf")
         ngrad = np.zeros(kernel_params.shape)
+
     return nll, ngrad
 
 def optimize_hyperparams(X, y, kernel, start_kernel_params, kernel_extra=None, kernel_priors=None):
@@ -221,18 +317,38 @@ def optimize_hyperparams(X, y, kernel, start_kernel_params, kernel_extra=None, k
 #    best_params = scipy.optimize.fmin_bfgs(f=ll, x0=start_kernel_params, fprime=grad)
 
     llgrad = lambda params: gp_nll_ngrad(X, y, kernel, params, kernel_extra, kernel_priors)
-    best_params, v, d = scipy.optimize.fmin_l_bfgs_b(func=llgrad, x0=start_kernel_params, bounds= [(0, None),]*len(start_kernel_params))
-    print "found best params", best_params
-    print "ll", v
-    print "d", d
+
+    skp = np.asarray(start_kernel_params)
+    new_params = lambda  :  np.exp(np.log(skp) + np.random.randn(len(skp)) * 2)
+    start_param_set = [skp,] + [new_params() for i in range(3)]
+
+    print "start param set"
+    print start_param_set
+
+    best_params = skp
+    nll, grad = llgrad(skp)
+    best_cost = nll
+
+    for params in start_param_set:
+
+#    best_params, v, d = scipy.optimize.fmin_l_bfgs_b(func=llgrad, x0=start_kernel_params, bounds= [(1e-20, None),]*len(start_kernel_params))
+        opt_params, v, d = scipy.optimize.fmin_l_bfgs_b(func=llgrad, x0=start_kernel_params, bounds= [(1e-20, None),]*len(start_kernel_params))
+
+        if v < best_cost:
+            best_cost = v
+            best_params = opt_params
+
 #    print "start ll", ll(start_kernel_params)
 #    print "best ll", ll(best_params)
+
+    print "OPTIMZIATION FINISHED: found best params", best_params
+    print "ll", v
     return best_params, v
 
 def plot_interpolated_surface(gp, X, y):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    
+
     xmin = np.min(X, 0)
     xmax = np.max(X, 0)
 
@@ -250,20 +366,20 @@ def plot_interpolated_surface(gp, X, y):
     #print xmin, xmax
     #print u, v
     #print x, y, k
-    
+
     ax.plot_surface(xc, yc, k,  color='b')
 
     plt.show()
 
 
-def gp_1D_predict( 
+def gp_1D_predict(
     gp,
     num_steps = 100,
     x_min = None,
     x_max = None,
     ):
     """Predict and plot the GP's predictions over the range provided"""
-    
+
     from pylab import figure, plot, show
     # print gp.X
     if None == x_min: x_min = min( x[0] for x in gp.X )
@@ -283,23 +399,23 @@ def gp_1D_predict(
 def gp_plot_prediction(predict_x, mean, variance = None):
     """
     Plot a gp's prediction using pylab including error bars if variance specified
-    
+
     Error bars are 2 * standard_deviation as in GP for ML book
     """
     from pylab import plot, concatenate, fill
     if None != variance:
         # check variances are just about +ve - could signify a bug if not
-        #assert variance.all() > -1e-10 
-        data = [ 
-            (x,y,max(v,0.0)) 
-            for x,y,v 
-            in zip( predict_x, mean.flat, variance  ) 
+        #assert variance.all() > -1e-10
+        data = [
+            (x,y,max(v,0.0))
+            for x,y,v
+            in zip( predict_x, mean.flat, variance  )
             ]
     else:
-        data = [ 
-            (x,y) 
-            for x,y 
-            in zip( predict_x, mean ) 
+        data = [
+            (x,y)
+            for x,y
+            in zip( predict_x, mean )
             ]
     data.sort( key = lambda d: d[0] ) # sort on X axis
     predict_x = [ d[0] for d in data ]
@@ -325,3 +441,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
--- kernels.py
+++ kernels.py
@@ -8,7 +8,6 @@ def gen_pairwise_matrix(f, X1, X2):
     if X1 is X2:
         for i in range(n1):
             for j in range(i, n2):
-#                print X1[i, :], X2[j,:]
                 K[i, j] = f(X1[i,:], X2[j,:])
         K = K + K.T - np.diag(np.diag(K))
     else:
@@ -38,6 +37,7 @@ class Kernel(object):
         elif len(X1.shape)==1 and len(X2.shape)==1 and X1.shape[0]==X2.shape[0]:
             return np.reshape(X1, (1, X1.shape[0])), np.reshape(X2, (1, X2.shape[0]))
         else:
+            print X1, X2
             raise RuntimeError("Incompatible dimensions in kernel arguments.")
 
     def __mul__(self, k_rhs):
@@ -59,10 +59,10 @@ class Kernel(object):
         raise RuntimeError( "Needs to be implemented in base class" )
 
     def param_prior_ll(self):
-        return np.sum([b.logpdf(a) for (a,b) in zip(self.params, self.priors)])
+        return np.sum([b.logpdf(a) if b is not None else 0 for (a,b) in zip(self.params, self.priors)])
 
     def param_prior_grad(self):
-        return np.asarray([b.logpdf_dx(a) for (a,b) in zip(self.params, self.priors)])
+        return np.asarray([b.logpdf_dx(a) if b is not None else 0 for (a,b) in zip(self.params, self.priors)])
 
 class SumKernel(Kernel):
     def __init__(self, lhs, rhs):
@@ -87,6 +87,42 @@ class SumKernel(Kernel):
         g = np.concatenate([self.lhs.param_prior_grad(), self.rhs.param_prior_grad()])
         return g
 
+class ProductKernel(Kernel):
+    def __init__(self, lhs, rhs):
+        self.lhs=lhs
+        self.rhs=rhs
+        self.nparams = lhs.nparams + rhs.nparams
+        self.param_split = lhs.nparams
+
+    def __call__(self, X1, X2):
+        return self.lhs(X1, X2)*self.rhs(X1, X2)
+
+    def derivative_wrt_i(self, i, X1, X2):
+        if i < self.lhs.nparams:
+            return self.lhs.derivative_wrt_i(i, X1, X2) * self.rhs(X1,X2)
+        else:
+            return self.rhs.derivative_wrt_i(i-self.lhs.nparams, X1,X2) * self.lhs(X1, X2)
+
+    def param_prior_ll(self):
+        return self.lhs.param_prior_ll() + self.rhs.param_prior_ll()
+
+    def param_prior_grad(self):
+        g = np.concatenate([self.lhs.param_prior_grad(), self.rhs.param_prior_grad()])
+        return g
+
+
+class LinearKernel(Kernel):
+
+    def __init__(self, params=None, priors=None):
+        super(LinearKernel, self).__init__(params, priors)
+
+    def __call__(self, X1, X2):
+        return np.dot(X1, X2.T)
+
+    def derivative_wrt_i(self, i, X1, X2):
+        (n,d) = X1.shape
+        (m,d) = X2.shape
+        return np.zeros((n,m))
 
 class SEKernel(Kernel):
 
@@ -134,31 +170,59 @@ class SEKernel(Kernel):
             raise RuntimeError("Unknown parameter index %d (out of %d) for SEKernel." % (i, self.nparams))
         return dK
 
-
 class DistFNKernel(Kernel):
-    def __init__(self, params, distfn, priors = None):
+    def __init__(self, params, distfn, priors = None, deriv=None):
         super(DistFNKernel, self).__init__(params, priors)
-        self.sigma_f = params[0]
-        self.w = params[1]
-        self.distfn = distfn
+
+        if len(params) == 1:
+            self.sigma_f = 1
+            self.w = params[0]
+            self.df_params = []
+        else:
+            self.sigma_f = params[0]
+            self.w = params[1]
+            self.df_params = params[2:]
+
+        self.distfn = lambda a,b: distfn(a,b,self.df_params)
+        self.distfn_deriv_i = None if deriv is None else lambda i, a, b: deriv(i, a, b, self.df_params)
 
     def __call__(self, X1, X2):
         X1, X2 = self._check_args(X1,X2)
+
+        if self.w == 0 or self.sigma_f ==0:
+            print "warning: invalid kernel parameter, returning 0", self.w, self.sigma_f
+            return np.zeros((X1.shape[0], X1.shape[0]))
+
         D = gen_pairwise_matrix(self.distfn, X1, X2)
-        return self.sigma_f**2 * np.exp(-1 * D**2 / self.w**2)
+        try:
+            d = self.sigma_f**2 * np.exp(-1 * D**2 / self.w**2)
+        except AttributeError:
+            import pdb
+            pdb.set_trace()
+        return d
 
     def derivative_wrt_i(self, i, X1, X2):
         X1, X2 = self._check_args(X1,X2)
-        D2 = gen_pairwise_matrix(self.distfn, X1, X2)**2
-        if i==0:
-            dK = 2*self.sigma_f * np.exp(-1 * D2 / self.w**2)
-        elif i == 1:
-            dK = 2 * self.sigma_f**2 * np.exp(-1*D2 / self.w**2) * D2 / (self.w**3)
+        D = gen_pairwise_matrix(self.distfn, X1, X2)
+
+        if self.w == 0 or self.sigma_f ==0:
+            print "warning: invalid kernel parameter, returning 0 matrix:", self.w, self.sigma_f
+            return np.zeros((X1.shape[0], X1.shape[0]))
+
+        if i==0 and len(self.params) > 1:
+            # deriv wrt sigma_f
+            dK = 2*self.sigma_f * np.exp(-1 * D**2 / self.w**2)
+        elif i == 1 or len(self.params) == 1:
+            # deriv wrt w
+            dK = 2 * self.sigma_f**2 * np.exp(-1*D**2 / self.w**2) * D**2 / (self.w**3)
+        elif i > 1:
+            dD = gen_pairwise_matrix(lambda x1, x2 : self.distfn_deriv_i(i-2, x1, x2), X1, X2)
+            dK = -2 * self.sigma_f**2 * np.exp(-1*D**2 / self.w**2) * D / (self.w**2) * dD
         else:
             raise RuntimeError("Unknown parameter index %d (out of %d) for DistFNKernel." % (i, self.nparams))
 
         return dK
-        
+
 
 class SEKernelIso(SEKernel):
     def __init__(self, params, priors = None):
@@ -209,12 +273,14 @@ class DiagonalKernel(Kernel):
 
     def __call__(self, X1, X2):
         X1, X2 = self._check_args(X1,X2)
+        (n,d) = X1.shape
         if X1 is X2:
-            (n,d) = X1.shape
             return self.s2 * np.eye(n)
         else:
-            f = lambda x1, x2: self.s2 if almost_equal(x1,x2) else 0
-            return gen_pairwise_matrix(f, X1, X2)
+            (m,d) = X2.shape
+            return np.zeros((n,m))
+#            f = lambda x1, x2: self.s2 if almost_equal(x1,x2) else 0
+#            return gen_pairwise_matrix(f, X1, X2)
 
     def derivative_wrt_i(self, i, X1, X2):
         X1, X2 = self._check_args(X1,X2)
@@ -232,7 +298,10 @@ def setup_kernel(name, params, extra, priors=None):
     if priors is None:
         priors = [None for p in params]
 
-    if name == "se":
+    if name == "linear":
+        sigma_n = params[0]
+        k = DiagonalKernel([sigma_n,], priors = priors[0]) + LinearKernel(params[1:], params[1:])
+    elif name == "se":
         sigma_n = params[0]
         k = DiagonalKernel([sigma_n,], priors = priors[0:1]) + SEKernel(params[1:], priors[1:])
     elif name == "se_iso":
@@ -244,8 +313,60 @@ def setup_kernel(name, params, extra, priors=None):
         sigma_n = params[0]
         sigma_f = params[1]
         w = params[2]
-        distfn = extra
-        k = DiagonalKernel([sigma_n,], priors = np.asarray(priors[0:1])) + DistFNKernel([sigma_f, w], distfn, priors = priors[1:])
+        if len(params) > 3:
+            distfn = extra[0]
+            distfn_deriv_i = extra[1]
+        else:
+            distfn = extra
+            distfn_deriv_i = None
+        distfn_params = list(params[3:])
+
+        k = DiagonalKernel([sigma_n,], priors = np.asarray(priors[0:1])) + DistFNKernel([sigma_f, w] + distfn_params, distfn, priors = priors[1:], deriv = distfn_deriv_i)
+
+    elif name == "distfns_sum":
+        # composite kernel of the form sum_i alpha_i *
+        # exp(-r_i(x1,x2)^2 / beta_i^2) where each r_i is a distance
+        # function and alpha_i and beta_i are magnitude and
+        # length-scale parameters respectively.
+
+        # here "params" is a list of 2n+1 entries. The first entry is
+        # sigma_n. The ith subsequent pair of entries gives alpha and
+        # beta for the ith distance function. "priors" has the same
+        # structure and semantics as "params".
+        pass
+        # "extra" is a list of n tuples of the form (distfn,
+        # distfn_deriv). distfn_deriv can be None if there are no
+        # special params to the distance function.
+
+        sigma_n = params[0]
+        composite_kernel = DiagonalKernel([sigma_n,], priors = [priors[0],])
+        for (i, (distfn, distfn_deriv_i)) in enumerate(extra):
+            si = 2*i+1
+            kparams = params[si:si+2]
+            kpriors = priors[si:si+2]
+            k = DistFNKernel(kparams, distfn, priors = kpriors, deriv = distfn_deriv_i)
+            composite_kernel += k
+        k = composite_kernel
+
+    elif name == "distfns_prod":
+        # similar to above, except that we construct a product of
+        # distfn kernels, so there is only a single sigma_f magnitude
+        # parameter (specified after sigma_n) instead of an alpha param
+        # for each kernel.
+
+        sigma_n = params[0]
+        noise_kernel = DiagonalKernel([sigma_n,], priors = [priors[0], ])
+
+        sigma_f = params[1]
+        composite_kernel = DistFNKernel([sigma_f, 1], lambda a,b,p : 0, priors = [priors[1],], deriv = None)
+        for (i, (distfn, distfn_deriv_i)) in enumerate(extra):
+            si = i+2
+            kparams = params[si:si+1]
+            kpriors = priors[si:si+1]
+            k = DistFNKernel(kparams, distfn, priors = kpriors, deriv = distfn_deriv_i)
+            composite_kernel *= k
+        k = composite_kernel + noise_kernel
+
     else:
         raise RuntimeError("unrecognized kernel name %s." % (name))
     return k
@@ -268,7 +389,11 @@ class Gamma(Distribution):
     def logpdf(self, x):
         alpha = self.alpha
         beta = self.beta
-        if 0.0 >= x: return np.log(1e-300)
+
+        if x < 0.0: return np.log(1e-300)
+        # the special case of an exponential distribution is defined even when x==0
+        if alpha == 1: return np.log(beta) - beta*x
+        if x == 0.0: return np.log(1e-300)
         lp = alpha*np.log(beta) - scipy.special.gammaln(alpha) + (alpha-1)*np.log(x) - beta*x
         if np.isnan(lp):
             lp = np.float("-inf")
@@ -278,7 +403,7 @@ class Gamma(Distribution):
         alpha = self.alpha
         beta = self.beta
         return (alpha-1)/x - beta
-        
+
 
 class InvGamma(Distribution):
     def __init__(self, alpha, beta):
@@ -291,14 +416,14 @@ class InvGamma(Distribution):
         lp = alpha*np.log(beta) - scipy.special.gammaln(alpha) - (alpha+1)*np.log(x) - beta/x
         if np.isnan(lp):
             lp = np.float("-inf")
-        
+
         return lp
 
     def logpdf_dx(self, x):
         alpha = self.alpha
         beta = self.beta
         return beta/(x**2) - (alpha+1)/x
-        
+
 class LogNormal(Distribution):
     def __init__(self, mu, sigma):
         self.mu = mu
