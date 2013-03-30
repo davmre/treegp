fix derivative calculations, and add (limited) test cases

gp.py
@@ -136,9 +136,9 @@ class GaussianProcess:
             ll1 = - .5 * ((y)**2 / var + np.log(2*np.pi*var) )
 
         L = scipy.linalg.cholesky(K, lower=True)
-        ld2 = np.log(np.diag(L)).sum()
+        ld2 = np.log(np.diag(L)).sum() # this computes .5 * log(det(K))
         alpha = scipy.linalg.cho_solve((L, True), y)
-        ll =  -.5 * (np.dot(y.T, alpha) + n * np.log(2*np.pi)) - ld2
+        ll =  -.5 * ( np.dot(y.T, alpha) + n * np.log(2*np.pi)) - ld2
         return ll
 
 
@@ -161,17 +161,20 @@ class GaussianProcess:
         for i,p in enumerate(self.kernel_params):
 
             dKdi = self.kernel.derivative_wrt_i(i, self.X, self.X)
-#            print "deriv wrt %d is" % (i), dKdi
             dlldi = .5 * np.dot(self.alpha.T, np.dot(dKdi, self.alpha))
-#            dlldi1 = dlldi
+
+            # here we use the fact:
+            # trace(AB) = sum_{ij} A_ij * B_ij
             dlldi -= .5 * np.sum(np.sum(self.Kinv.T * dKdi))
+
+
 #            print "dlldi is %f = .5 * %f - .5 * %f" % (dlldi, dlldi1*2, (dlldi1-dlldi)*2)
 #            print "norm2 alpha is %f" % (np.dot(self.alpha,  self.alpha))
 #            alt = .5 * np.dot(self.y.T, np.dot(self.Kinv, np.dot(dKdi, np.dot(self.Kinv, self.y))))
 #            print "alt is %f" % (alt)
 
             grad[i] = dlldi
-#        print self.kernel_params, "returning grad", grad
+ #       print self.kernel_params, "returning grad", grad
         return grad
 
     def save_trained_model(self, filename):

kernels.py
@@ -228,15 +228,17 @@ class DistFNKernel(Kernel):
             print "warning: invalid kernel parameter, returning 0 matrix:", self.w, self.sigma2_f
             return np.zeros((X1.shape[0], X1.shape[0]))
 
+        # import pdb; pdb.set_trace()
+
         if i==0 and len(self.params) > 1:
             # deriv wrt sigma2_f
             dK = np.exp(-1 * D**2 / self.w**2)
         elif i == 1 or len(self.params) == 1:
             # deriv wrt w
-            dK = self.sigma2_f * np.exp(-1*D**2 / self.w**2) * D**2 / (self.w**3)
+            dK = self.sigma2_f * np.exp(-1*D**2 / self.w**2) * 2 * D**2 / (self.w**3)
         elif i > 1:
             dD = gen_pairwise_matrix(lambda x1, x2 : self.distfn_deriv_i(i-2, x1, x2), X1, X2)
-            dK = -self.sigma2_f * np.exp(-1*D**2 / self.w**2) * D / (self.w**2) * dD
+            dK = -self.sigma2_f * np.exp(-1*D**2 / self.w**2) * 2 * D / (self.w**2) * dD
         else:
             raise RuntimeError("Unknown parameter index %d (out of %d) for DistFNKernel." % (i, self.nparams))
 
@@ -286,12 +288,11 @@ class SEKernelIso(SEKernel):
 class DiagonalKernel(Kernel):
     """
     Kernel given by k(x1,x2)=s^2 if x1==x2, otherwise =0. Takes a
-    single parameter, s.
+    single parameter, s^2.
     """
     def __init__(self, params, priors=None):
         super(DiagonalKernel, self).__init__(params, priors)
         self.s2 = self.params[0]
-        self.s = np.sqrt(self.s2)
 
     def __call__(self, X1, X2):
         X1, X2 = self._check_args(X1,X2)
@@ -311,9 +312,9 @@ class DiagonalKernel(Kernel):
         else:
             if X1 is X2:
                 (n,d) = X1.shape
-                return 2 * self.s * np.eye(n)
+                return np.eye(n)
             else:
-                f = lambda x1, x2: 2*self.s if almost_equal(x1,x2) else 0
+                f = lambda x1, x2: 1 if almost_equal(x1,x2) else 0
                 return gen_pairwise_matrix(f, X1, X2)
 
 def setup_kernel(name, params, extra, priors=None):

learn.py
@@ -56,7 +56,7 @@ def gp_nll_ngrad(X, y, kernel, kernel_params, kernel_extra, kernel_priors):
         ngrad += npgrad
 
         print "   ...grad", -1*ngrad
-        print "   ...ll", -1 *nll, 
+        print "   ...ll", -1 *nll,
 
     except np.linalg.linalg.LinAlgError as e:
         print "warning: lin alg error (%s) in likelihood computation, returning likelihood -inf" % str(e)

test/test.py
@@ -4,7 +4,9 @@ import numpy as np
 
 from gpr import munge, kernels, evaluate, learn, distributions, plot
 from gpr.gp import GaussianProcess
+from gpr.distributions import InvGamma, LogNormal
 
+"""
 class TestAbalone(unittest.TestCase):
 
     def setUp(self):
@@ -23,7 +25,7 @@ class TestAbalone(unittest.TestCase):
 
         self.X = X
         self.y = y
-
+"""
 
 class TestSimple(unittest.TestCase):
 
@@ -57,11 +59,53 @@ class TestSimple(unittest.TestCase):
         loss = evaluate.test_kfold(self.X, self.y, folds=5, kernel="se_iso", kernel_params=(0.1, 1, 22.0))
 
     def test_learn_hyperparams(self):
-        from gpr.distributions import InvGamma, LogNormal
         priors = [InvGamma(1.0, 1.0), InvGamma(1.0, 1.0), LogNormal(3.0, 2.0)]
         best_params, v = learn.learn_hyperparams(self.X, self.y, "se", start_kernel_params = self.start_params, kernel_priors=priors)
 
         print best_params
+
+    def test_SE_gradient(self):
+
+        kernel = "se"
+        grad = learn.gp_grad(X=self.X, y=self.y, kernel=kernel, kernel_params=self.start_params, kernel_extra=None)
+
+        n = len(self.start_params)
+        kp  = self.start_params
+        eps = 1e-4
+        empirical_grad = np.zeros(n)
+        for i in range(n):
+            kp[i] -= eps
+            gp = GaussianProcess(X=self.X, y=self.y, kernel=kernel, kernel_params=kp)
+            l1 = gp.log_likelihood()
+            kp[i] += 2*eps
+            gp = GaussianProcess(X=self.X, y=self.y, kernel=kernel, kernel_params=kp)
+            l2 = gp.log_likelihood()
+            kp[i] -= eps
+            empirical_grad[i] = (l2 - l1)/ (2*eps)
+
+        self.assertTrue( (np.abs(grad - empirical_grad) < 0.01 ).all() )
+
+    def test_prior_gradient(self):
+        kernel = "se"
+
+        k = kernels.setup_kernel(name=kernel, params=self.start_params, extra=None, priors = [InvGamma(1.0, 1.0), InvGamma(1.0, 1.0), LogNormal(3.0, 2.0)])
+        pgrad = k.param_prior_grad()
+
+        n = len(self.start_params)
+        kp  = self.start_params
+        eps = 1e-4
+        empirical_pgrad = np.zeros(n)
+        for i in range(n):
+            kp[i] -= eps
+            k = kernels.setup_kernel(name=kernel, params=kp, extra=None, priors = [InvGamma(1.0, 1.0), InvGamma(1.0, 1.0), LogNormal(3.0, 2.0)])
+            l1 = k.param_prior_ll()
+            kp[i] += 2*eps
+            k = kernels.setup_kernel(name=kernel, params=kp, extra=None, priors = [InvGamma(1.0, 1.0), InvGamma(1.0, 1.0), LogNormal(3.0, 2.0)])
+            l2 = k.param_prior_ll()
+            kp[i] -= eps
+            empirical_pgrad[i] = (l2 - l1)/ (2*eps)
+
+        self.assertTrue( (np.abs(pgrad - empirical_pgrad) < 0.01 ).all() )
 
     def test_plot(self):
         plot.predict_1d(self.gp, x_min = -5.0, x_max = 5.0)