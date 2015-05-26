0762f5f7cfbb4452b0a2e66430fcfe5c3ca36628
David Moore <dmoore@cs.berkeley.edu>
2015-05-26 12:38:55 -0700

    add factored gplvm code

---
+++ bcm/bcmopt.py
@@ -0,0 +1,275 @@
+from treegp.bcm.multi_shared_bcm import MultiSharedBCM, Blocker, sample_synthetic, sample_synthetic_bcm
+from treegp.bcm.local_regression import BCM
+
+from treegp.gp import GPCov, GP, mcov, prior_sample, dgaussian
+from treegp.util import mkdir_p
+import numpy as np
+import scipy.stats
+import scipy.optimize
+import time
+import os
+import sys
+import cPickle as pickle
+
+EXP_DIR = os.path.join(os.environ["HOME"], "bcmopt_experiments")
+mkdir_p(EXP_DIR)
+
+class SampledData(object):
+
+    def __init__(self, old_sdata=None,
+                 noise_var=0.01, n=30, ntrain=20, lscale=0.5,
+                 obs_std=0.05, yd=10, centers=None, seed=1, samplebcm=False):
+        self.noise_var=noise_var
+        self.n = n
+        self.ntrain = ntrain
+        self.lscale=lscale
+
+        if old_sdata is not None:
+            self.cov = old_sdata.cov
+            self.Xtest, self.Ytest = old_sdata.Xtest, old_sdata.Ytest
+            X, Y = old_sdata.SX, old_sdata.SY
+        else:
+            if samplebcm:
+                b = Blocker(centers)
+                Xfull, Yfull, cov = sample_synthetic_bcm(n=n, noise_var=noise_var, yd=yd, lscale=lscale, seed=seed, blocker = b)
+            else:
+                Xfull, Yfull, cov = sample_synthetic(n=n, noise_var=noise_var, yd=yd, lscale=lscale, seed=seed)
+            self.cov = cov
+            X, Y = Xfull[:ntrain,:], Yfull[:ntrain,:]
+            self.Xtest, self.Ytest = Xfull[ntrain:,:], Yfull[ntrain:,:]
+
+        if centers is not None:
+            b = Blocker(centers)
+            self.SX, self.SY, self.perm, self.block_boundaries = b.sort_by_block(X, Y)
+            self.centers = centers
+        else:
+            self.SX, self.SY = X, Y
+            self.block_boundaries = [(0, X.shape[0])]
+            self.centers = [np.array((0.0, 0.0))]
+
+        self.obs_std = obs_std
+        self.X_obs = self.SX + np.random.randn(*X.shape)*obs_std
+
+    def build_mbcm(self):
+        mbcm = MultiSharedBCM(self.SX, Y=self.SY, block_boundaries=self.block_boundaries, cov=self.cov,
+                              noise_var=self.noise_var, kernelized=False, neighbor_threshold=1e-3)
+        return mbcm
+
+    def mean_abs_err(self, x):
+        return np.mean(np.abs(x - self.SX.flatten()))
+
+    def prediction_error_gp(self, x):
+        XX = x.reshape(self.X_obs.shape)
+        ll = 0
+        for y, yt in zip(self.SY.T, self.Ytest.T):
+            gp = GP(X=XX, y=y, cov_main=self.cov, noise_var=self.noise_var,
+                    sort_events=False, sparse_invert=False)
+            pred_means = gp.predict(self.Xtest)
+            pred_cov = gp.covariance(self.Xtest, include_obs=True)
+            lly = scipy.stats.multivariate_normal(pred_means, pred_cov).logpdf(yt)
+            ll += lly
+        return ll
+
+    def prediction_error_bcm(self, x):
+        XX = x.reshape(self.X_obs.shape)
+        ll = 0
+        for y, yt in zip(self.SY.T, self.Ytest.T):
+            bcm = BCM(block_centers=self.centers, cov_block_params = [(self.noise_var, 1.0, self.lscale, self.lscale)],
+                      X=XX, y=y, test_cov=self.cov)
+            m, c = bcm.predict_dist(self.Xtest, noise_var=self.noise_var)
+            lly = scipy.stats.multivariate_normal(m, c).logpdf(yt)
+            ll += lly
+        return ll
+
+    def x_prior(self, xx):
+        flatobs = self.X_obs.flatten()
+        ll = np.sum([scipy.stats.norm(flatobs[i], scale=self.obs_std).logpdf(xx[i]) for i in range(len(xx))])
+        lderiv = np.array([-(xx[i]-flatobs[i])/(self.obs_std**2) for i in range(len(xx))]).flatten()
+        return ll, lderiv
+
+    def random_init(self, jitter_std=None):
+        if jitter_std is None:
+            jitter_std = self.obs_std
+        return self.X_obs + np.random.randn(*self.X_obs.shape)*jitter_std
+
+
+llgrad_bcm = lambda mbcm : mbcm.llgrad(local=True, grad_X=True)
+llgrad_lgp = lambda mbcm : mbcm.llgrad_blocked(grad_X=True)
+llgrad_gp = lambda mbcm : mbcm.llgrad(grad_X=True)
+
+def do_optimization(llgrad, mbcm, run_name, X0, sdata, method, maxiter=200):
+
+    x0 = X0.flatten()
+
+    sstep = [0,]
+    d = EXP_DIR + run_name
+    mkdir_p(d)
+
+    with open(os.path.join(d, "data.pkl"), 'wb') as f:
+        pickle.dump(sdata, f)
+
+    f_log = open(os.path.join(d, "log.txt"), 'w')
+    t0 = time.time()
+
+    def lgpllgrad(xx):
+        XX = xx.reshape(X0.shape)
+        mbcm.update_X(XX)
+        #ll, grad = mbcm.llgrad(local=True, parallel=True, grad_X=True)
+        ll, grad = llgrad(mbcm)
+        grad = grad.flatten()
+
+        prior_ll, prior_grad = sdata.x_prior(xx)
+        ll += prior_ll
+        grad += prior_grad
+
+        print "%d %.2f %.2f" % (sstep[0], time.time()-t0, ll)
+        f_log.write("%d %.2f %.2f\n" % (sstep[0], time.time()-t0, ll))
+        f_log.flush()
+
+        np.save(os.path.join(d, "step_%05d" % sstep[0]), XX)
+        sstep[0] += 1
+
+        return -ll, -grad
+
+    bounds = [(0.0, 1.0),]*len(x0)
+    r = scipy.optimize.minimize(lgpllgrad, x0, jac=True, method=method, bounds=bounds, options={'maxiter': maxiter})
+    t1 = time.time()
+
+    np.save(os.path.join(d, "step_final"), r.x.reshape(X0.shape))
+
+    f_log.close()
+
+    with open(os.path.join(d, "results.txt"), 'w') as f:
+        f.write("optimized in %.2f seconds\n" % (t1-t0))
+        f.write("MAD error %.04f to %.04f\n" % (sdata.mean_abs_err(x0), sdata.mean_abs_err(r.x)))
+        f.write("GP predictive likelihood %.3f to %.3f (true %.3f)\n" % (sdata.prediction_error_gp(x0),
+                                                                         sdata.prediction_error_gp(r.x),
+                                                                         sdata.prediction_error_gp(sdata.SX.flatten())))
+        f.write("BCM predictive likelihood %.3f to %.3f (true %.3f)\n" % (sdata.prediction_error_bcm(x0),
+                                                                          sdata.prediction_error_bcm(r.x),
+                                                                          sdata.prediction_error_bcm(sdata.SX.flatten())))
+        f.write("\n\nresult:\n")
+        f.write(str(r))
+        f.write("\n")
+
+
+def run_multi(sdata,run_name,  llgrad, nrestarts):
+    mbcm = data.build_mbcm()
+
+
+def do_run(run_name, lscale, n, ntrain, yd, old_sdata=None,
+           fullgp=False, restarts=1, method=None, samplebcm=False):
+    if fullgp:
+        centers = None
+        llgrad=llgrad_gp
+    else:
+        pmax = np.ceil(np.sqrt(np.sqrt(ntrain)))*2+1
+        pts = np.linspace(0, 1, pmax)[1::2]
+        centers = [np.array((xx, yy)) for xx in pts for yy in pts]
+        print "bcm with %d blocks" % (len(centers))
+        llgrad=llgrad_bcm
+
+    data = SampledData(noise_var=0.01, n=n,
+                       ntrain=ntrain, lscale=lscale,
+                       obs_std=0.05, yd=yd,
+                       centers=centers,
+                       old_sdata=old_sdata,
+                       samplebcm=samplebcm)
+    mbcm = data.build_mbcm()
+
+    for i in range(restarts):
+        X0 = data.random_init()
+        do_optimization(llgrad, mbcm, "%s_init%02d" % (run_name, i), X0, data, method=method)
+
+def savesample(run_name, n, ntrain, lscale, yd):
+    data = SampledData(noise_var=0.01, n=n,
+                       ntrain=ntrain, lscale=lscale,
+                       obs_std=0.05, yd=30,
+                       centers=centers)
+
+    d = EXP_DIR + run_name
+    with open(os.path.join(d, "data.pkl"), 'rb') as f:
+        old_sdata = pickle.dump(data, f)
+
+
+def main():
+
+
+
+    pts = np.linspace(0, 1, 9)[1::2]
+    centers = [np.array((xx, yy)) for xx in pts for yy in pts]
+    #centers = [np.array((0.5, 0.5))]
+    #centers = None
+
+    #data = SampledData(noise_var=0.01, n=4200,
+    #                   ntrain=4000, lscale=0.02,
+    #                   obs_std=0.05, yd=30,
+    #                   centers=centers)
+    run_name = "low_lscale_4k"
+    d = EXP_DIR + run_name
+    with open(os.path.join(d, "data.pkl"), 'rb') as f:
+        old_sdata = pickle.load(f)
+
+    data = SampledData(old_sdata,
+                       noise_var=0.01, n=4200,
+                       ntrain=4000, lscale=0.02,
+                       obs_std=0.05, yd=30,
+                       centers=centers)
+    mbcm = data.build_mbcm()
+
+    X0 = data.X_obs.copy()
+    do_optimization(llgrad_bcm, mbcm, run_name, X0, data, method="l-bfgs-b")
+
+
+def run_experiments():
+    """
+    experiments = [ (False, 400, 500, 0.05, 50, 'tnc', 3),
+                    (False, 400, 500, 0.05, 5, 'l-bfgs-b', 3),
+                   (False, 400, 500, 0.05, 10, 'l-bfgs-b', 3),
+                   (False, 400, 500, 0.05, 50, 'l-bfgs-b', 3),
+
+                   (True, 400, 500, 0.05, 5, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.05, 10, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.05, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.05, 50, 'tnc', 3),
+                   (True, 400, 500, 0.1, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.5, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.01, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.1, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.5, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.01, 50, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 5, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 10, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 50, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 50, 'tnc', 3)]"""
+    experiments = [(True, 400, 500, 0.5, 50, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.5, 10, 'l-bfgs-b', 3),
+                   (True, 400, 500, 0.5, 5, 'l-bfgs-b', 3),
+                   (False, 400, 500, 0.5, 50, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 5, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 10, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 50, 'l-bfgs-b', 3),
+                   (False, 5000, 5200, 0.02, 50, 'tnc', 3),
+                   (False, 5000, 5200, 0.05, 50, 'l-bfgs-b', 3)]
+
+    for (fullgp, ntrain, n, lscale, yd, method, inits) in experiments:
+        run_name = "%d_%d_%.2f_%d_%s_%d_%s" % (ntrain, n, lscale, yd, method, inits, fullgp)
+        print "running", run_name
+        try:
+            do_run(run_name=run_name, lscale=lscale, n=n, ntrain=ntrain, yd=yd, fullgp=fullgp, restarts=inits, method=method)
+        except Exception as e:
+            print e
+            continue
+
+
+if __name__ == "__main__":
+    ntrain = int(sys.argv[1])
+    n = int(sys.argv[2])
+    lscale = float(sys.argv[3])
+    yd = int(sys.argv[4])
+    method = sys.argv[5]
+    inits =int(sys.argv[6])
+    fullgp = sys.argv[7].startswith('t')
+
+    run_name = "%d_%d_%.2f_%d_%s_%d_%s" % (ntrain, n, lscale, yd, method, inits, fullgp)
+    do_run(run_name=run_name, lscale=lscale, n=n, ntrain=ntrain, yd=yd, fullgp=fullgp, restarts=inits, method=method, samplebcm=True)
---
+++ bcm/multi_shared_bcm.py
@@ -0,0 +1,382 @@
+import numpy as np
+import scipy.stats
+
+from collections import defaultdict
+from multiprocessing import Pool
+
+from treegp.gp import GP, GPCov, mcov, dgaussian, dgaussian_rank1
+
+from treegp.cover_tree import VectorTree
+import pyublas
+
+
+class Blocker(object):
+
+    def __init__(self, block_centers):
+        self.block_centers = block_centers
+        self.n_blocks = len(block_centers)
+
+    def get_block(self, X_new):
+        dists = [np.linalg.norm(X_new - center) for center in self.block_centers]
+        return np.argmin(dists)
+
+    def sort_by_block(self, X, Y=None, YY=None):
+        if Y is not None:
+            assert(Y.shape[0] == X.shape[0])
+        elif YY is not None:
+            assert(YY.shape[0] == X.shape[0])
+            assert(YY.shape[1] == X.shape[0])
+
+        n = X.shape[0]
+        blocks = np.array([self.get_block(xp) for xp in X])
+        idxs = np.arange(n)
+        perm = np.asarray(sorted(idxs, key = lambda i : blocks[i]))
+        sorted_X = X[perm]
+        sorted_blocks = blocks[perm]
+        block_idxs = [idxs[sorted_blocks==block] for block in np.arange(self.n_blocks)]
+        block_boundaries = [ (np.min(bi), np.max(bi)+1)  for bi in block_idxs  ]
+
+        if Y is not None:
+            sorted_Y = Y[perm]
+            return sorted_X, sorted_Y, perm, block_boundaries
+        elif YY is not None:
+            tmp = YY[perm]
+            sorted_YY = tmp[:, perm]
+            return sorted_X, sorted_YY, perm, block_boundaries
+        else:
+            return sorted_X, perm, block_boundaries
+
+def sample_synthetic(seed=1, n=400, xd=2, yd=10, lscale=0.1, noise_var=0.01):
+    # sample data from the prior
+    np.random.seed(seed)
+    X = np.random.rand(n, xd)
+
+    cov = GPCov(wfn_params=[1.0], dfn_params=[lscale, lscale], dfn_str="euclidean", wfn_str="se")
+    KK = mcov(X, cov, noise_var)
+
+    y = scipy.stats.multivariate_normal(mean=np.zeros((X.shape[0],)), cov=KK).rvs(yd).T.reshape((-1, yd))
+
+    return X, y, cov
+
+def sample_synthetic_bcm(seed=1, n=400, xd=2, yd=10, lscale=0.1, noise_var=0.01, blocker=None):
+    # sample data from the prior
+    np.random.seed(seed)
+    X = np.random.rand(n, xd)
+
+    SX, perm, block_boundaries = blocker.sort_by_block(X)
+    cov = GPCov(wfn_params=[1.0], dfn_params=[lscale, lscale], dfn_str="euclidean", wfn_str="se")
+
+    Y = []
+    for d in range(yd):
+        gps = []
+        yis = []
+        for block, (i_start, i_end) in enumerate(block_boundaries):
+            Xi = SX[i_start:i_end, :]
+            Ki = mcov(Xi, cov, noise_var)
+
+            means = [gp.predict(Xi) for gp in gps]
+            covs = [gp.covariance(Xi, include_obs=True) for gp in gps]
+            precs = [np.linalg.inv(ccov) for ccov in covs]
+            combined_prec = np.sum(precs, axis=0)
+
+            target_prior_cov = mcov(Xi, cov, noise_var)
+            target_prior_prec = np.linalg.inv(target_prior_cov)
+            prior_covs = [mcov(Xi, lgp.cov_main, lgp.noise_var) for lgp in gps]
+            prior_precs = [np.linalg.inv(ccov) for ccov in prior_covs]
+            combined_prec += target_prior_prec
+            combined_prec -= np.sum(prior_precs, axis=0)
+            ccov = np.linalg.inv(combined_prec)
+
+            weighted_means = [np.dot(prec, mean) for (mean, prec) in zip(means, precs)]
+            if len(weighted_means) > 0:
+                mean = np.dot(ccov, np.sum(weighted_means, axis=0))
+            else:
+                mean = np.zeros((ccov.shape[0],))
+
+            yi = scipy.stats.multivariate_normal(mean=mean, cov=ccov).rvs(1)
+
+            ngp = GP(X=Xi, y=yi, cov_main=cov, noise_var=noise_var, sort_events=False,
+                     sparse_invert=False, compute_ll=False)
+            gps.append(ngp)
+            yis.append(yi)
+            print "  sampled block", block
+        Yd = np.concatenate(yis).reshape((-1, 1))
+        print "sampled all blocks for dimension", d
+        Y.append(Yd)
+    Y = np.hstack(Y)
+
+    return X, Y, cov
+
+
+
+class MultiSharedBCM(object):
+
+    def __init__(self, X, Y, block_boundaries, cov, noise_var, kernelized=False, dy=None, neighbor_threshold=1e-5):
+        self.X = X
+
+        if kernelized:
+            self.kernelized = True
+            self.YY = Y
+            assert(dy is not None)
+            self.dy = dy
+        else:
+            self.kernelized = False
+            self.Y = Y
+        self.block_boundaries = block_boundaries
+        self.n_blocks = len(block_boundaries)
+
+        self.cov = cov
+        self.noise_var = noise_var
+        dummy_X = np.array([[0.0,] * self.X.shape[1],], dtype=float)
+        self.predict_tree = VectorTree(dummy_X, 1, cov.dfn_str, cov.dfn_params, cov.wfn_str, cov.wfn_params)
+
+        self.compute_neighbors(threshold=neighbor_threshold)
+        self.neighbor_threshold = neighbor_threshold
+
+    def compute_neighbors(self, threshold=1e-5):
+        neighbor_count = defaultdict(int)
+        neighbors = []
+        for i in range(self.n_blocks):
+            i_start, i_end = self.block_boundaries[i]
+            Xi = self.X[i_start:i_end]
+            ni = Xi.shape[0]
+            for j in range(i):
+                j_start, j_end = self.block_boundaries[j]
+                Xj = self.X[j_start:j_end]
+                Kij = self.kernel(Xi, X2=Xj)
+                maxk = np.max(np.abs(Kij))
+                if maxk > threshold:
+                    neighbors.append((i,j))
+                    neighbor_count[i] += 1
+                    neighbor_count[j] += 1
+        self.neighbor_count = neighbor_count
+        self.neighbors = neighbors
+
+    def update_X(self, new_X, recompute_neighbors=False):
+        self.X = new_X
+        if recompute_neighbors:
+            self.compute_neighbors(threshold=self.neighbor_threshold)
+
+    def llgrad_blocked(self, parallel=False, **kwargs):
+        """
+        Compute likelihood under a model with blocked local GPs (no pairwise corrections)
+        """
+        if parallel:
+            pool = Pool(processes=4)
+            unary_args = [(kwargs, self, i) for i in range(self.n_blocks)]
+            unaries = pool.map(llgrad_unary_shim, unary_args)
+        else:
+            unaries = [self.llgrad_unary(i, **kwargs) for i in range(self.n_blocks)]
+
+        unary_lls, unary_grads = zip(*unaries)
+
+        ll = np.sum(unary_lls)
+
+        if "grad_X" in kwargs and kwargs['grad_X']:
+            grads = np.zeros(self.X.shape)
+        else:
+            grads = np.zeros((0, 0))
+
+        for i in range(self.n_blocks):
+            i_start, i_end = self.block_boundaries[i]
+            grads[i_start:i_end, :] += unary_grads[i]
+
+        return ll, grads
+
+    def llgrad(self, parallel=False, local=True, **kwargs):
+        # overall likelihood is the pairwise potentials for all (unordered) pairs,
+        # where each block is involved in (n-1) pairs. So we subtract each unary potential n-1 times.
+        # Then finally each unary potential gets added in once.
+
+        if local:
+            neighbors = self.neighbors
+            neighbor_count = self.neighbor_count
+        else:
+            neighbors = [(i,j) for i in range(self.n_blocks) for j in range(i)]
+            neighbor_count = dict([(i, self.n_blocks-1) for i in range(self.n_blocks)])
+
+        if parallel:
+            #pool = Pool(processes=4)
+            unary_args = [(kwargs, self, i) for i in range(self.n_blocks)]
+            unaries = pool.map(llgrad_unary_shim, unary_args)
+
+            pair_args = [(kwargs, self, i, j) for (i,j) in neighbors]
+            pairs = pool.map(llgrad_joint_shim, pair_args)
+        else:
+            unaries = [self.llgrad_unary(i, **kwargs) for i in range(self.n_blocks)]
+            pairs = [self.llgrad_joint(i, j, **kwargs) for (i,j) in neighbors]
+
+        unary_lls, unary_grads = zip(*unaries)
+        if len(pairs) > 0:
+            pair_lls, pair_grads = zip(*pairs)
+        else:
+            pair_lls = []
+            pair_grads = []
+
+        ll = np.sum(pair_lls)
+        ll -= np.sum([(neighbor_count[i]-1)*ull for (i, ull) in enumerate(unary_lls) ])
+
+        if "grad_X" in kwargs and kwargs['grad_X']:
+            grads = np.zeros(self.X.shape)
+        else:
+            grads = np.zeros((0, 0))
+
+        pair_idx = 0
+        for i in range(self.n_blocks):
+            i_start, i_end = self.block_boundaries[i]
+            grads[i_start:i_end, :] -= (neighbor_count[i]-1)*unary_grads[i]
+
+        for pair_idx, (i,j) in enumerate(neighbors):
+            i_start, i_end = self.block_boundaries[i]
+            j_start, j_end = self.block_boundaries[j]
+            ni = i_end-i_start
+            grads[i_start:i_end] += pair_grads[pair_idx][:ni]
+            grads[j_start:j_end] += pair_grads[pair_idx][ni:]
+        return ll, grads
+
+
+    def llgrad_unary(self, i, **kwargs):
+        i_start, i_end = self.block_boundaries[i]
+        X = self.X[i_start:i_end]
+
+        if self.kernelized:
+            YY = self.YY[i_start:i_end, i_start:i_end]
+            return self.gaussian_llgrad_kernel(X, YY, dy=self.dy, **kwargs)
+        else:
+            Y = self.Y[i_start:i_end, :]
+            return self.gaussian_llgrad(X, Y, **kwargs)
+
+    def llgrad_joint(self, i, j, **kwargs):
+        i_start, i_end = self.block_boundaries[i]
+        j_start, j_end = self.block_boundaries[j]
+        Xi = self.X[i_start:i_end]
+        Xj = self.X[j_start:j_end]
+
+        ni = Xi.shape[0]
+        nj = Xj.shape[0]
+        X = np.vstack([Xi, Xj])
+
+        if self.kernelized:
+            YY = np.empty((ni+nj, ni+nj))
+            YY[:ni, :ni] = self.YY[i_start:i_end, i_start:i_end]
+            YY[ni:, ni:] = self.YY[j_start:j_end, j_start:j_end]
+            YY[:ni, ni:] = self.YY[i_start:i_end, j_start:j_end]
+            YY[ni:, :ni]  = YY[:ni, ni:].T
+            return self.gaussian_llgrad_kernel(X, YY, dy=self.dy, **kwargs)
+        else:
+            Yi = self.Y[i_start:i_end, :]
+            Yj = self.Y[j_start:j_end, :]
+            Y = np.vstack([Yi, Yj])
+            return self.gaussian_llgrad(X, Y, **kwargs)
+
+
+    def kernel(self, X, X2=None):
+        if X2 is None:
+            n = X.shape[0]
+            K = self.predict_tree.kernel_matrix(X, X, False)
+            K += np.eye(n) * self.noise_var
+        else:
+            K = self.predict_tree.kernel_matrix(X, X2, False)
+        return K
+
+    def dKdx(self, X, p, i, return_vec=False):
+        # derivative of kernel(X1, X2) wrt i'th coordinate of p'th point in X1.
+        if return_vec:
+            dKv = self.predict_tree.kernel_deriv_wrt_xi_row(X, p, i)
+            dKv[p] = 0
+            return dKv
+        else:
+            dK = self.predict_tree.kernel_deriv_wrt_xi(X, X, p, i)
+            dK[p,p] = 0
+            dK = dK + dK.T
+            return dK
+
+    def gaussian_llgrad(self, X, Y, grad_X = False):
+        n, dx = X.shape
+        dy = Y.shape[1]
+
+        K = mcov(X, self.cov, self.noise_var)
+        prec = np.linalg.inv(K)
+        Alpha = np.dot(prec, Y)
+
+        ll = -.5 * np.sum(Y*Alpha)
+        ll += -.5 * dy * np.linalg.slogdet(K)[1]
+        ll += -.5 * dy * n * np.log(2*np.pi)
+
+        if not grad_X:
+            return ll, np.array(())
+
+        llgrad = np.zeros((n, dx))
+        for p in range(n):
+            for i in range(dx):
+                dll = 0
+                dcv = self.dKdx(X, p, i, return_vec=True)
+                #t1 = -np.outer(prec[p,:], dcv)
+                #t1[:, p] = -np.dot(prec, dcv)
+                #dll_dcov = .5*ny*np.trace(t1)
+
+                dll = -dy * np.dot(prec[p,:], dcv)
+
+                for j in range(dy):
+                    alpha = Alpha[:,j]
+                    dK_alpha = dcv * alpha[p]
+                    dK_alpha[p] = np.dot(dcv, alpha)
+                    dll_dcov = .5*np.dot(alpha, dK_alpha)
+                    dll += dll_dcov
+
+                llgrad[p,i] = dll
+
+        return ll, llgrad
+
+    def gaussian_llgrad_kernel(self, X, YY, dy=None, grad_X=False):
+        n, dx = X.shape
+        if dy is None:
+            dy = self.dy
+
+        K = mcov(X, self.cov, self.noise_var)
+        Kinv = np.linalg.inv(K)
+        prec = Kinv
+
+        KYYK = np.dot(np.dot(Kinv, YY), Kinv)
+
+        ll =  -.5 * np.sum(Kinv * YY)
+        ll += -.5 * dy * np.linalg.slogdet(K)[1]
+        ll += -.5 * dy * n * np.log(2*np.pi)
+        if not grad_X:
+            return ll, np.array(())
+
+        llgrad = np.zeros((n, dx))
+        for p in range(n):
+            for i in range(dx):
+                #dcv_full = self.dKdx(X, p, i)
+                #dll = -.5*np.sum(KYYK * dcv_full)
+                dcv = self.dKdx(X, p, i, return_vec=True)
+                dll = np.dot(KYYK[p,:], dcv)
+
+                dll += -dy * np.dot(prec[p,:], dcv)
+
+                llgrad[p,i] = dll
+
+                #t1 = -np.outer(prec[p,:], dcov_v)
+                #t1[:, p] = -np.dot(prec, dcov_v)
+
+        return ll, llgrad
+
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        del d['predict_tree']
+        return d
+
+    def __setstate__(self, d):
+        self.__dict__ = d
+        dummy_X = np.array([[0.0,] * self.X.shape[1],], dtype=float)
+        self.predict_tree = VectorTree(dummy_X, 1, self.cov.dfn_str, self.cov.dfn_params, self.cov.wfn_str, self.cov.wfn_params)
+
+from multiprocessing import Pool
+import time
+
+def llgrad_unary_shim(arg):
+    return MultiSharedBCM.llgrad_unary(*arg[1:], **arg[0])
+
+def llgrad_joint_shim(arg):
+    return MultiSharedBCM.llgrad_joint(*arg[1:], **arg[0])
